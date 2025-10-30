556cac524c325b7c7ebc9af4a066d8ebf6ce4833
naman-1262 <202301423@dau.ac.in>
2025-10-30 21:16:46 +0530

    Implementing the coupon api

---
+++ Backend/routes/coupon.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { protect, isAdmin } = require('../middlewares/auth');
+const couponController = require('../controllers/couponController');
+
+router.post('/apply', protect, couponController.applyCoupon);
+router.get('/', protect, couponController.getCoupons);
+router.post('/', protect, isAdmin, couponController.createCoupon);
+router.put('/:id', protect, isAdmin, couponController.updateCoupon);
+router.delete('/:id', protect, isAdmin, couponController.deleteCoupon);
+
+module.exports = router;
