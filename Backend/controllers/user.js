7a8c0be185c945ed03993688671b6809490ebd24
naman-1262 <202301423@dau.ac.in>
2025-11-29 12:57:24 +0530

    making the reset password work by using brevo smtp

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -6,24 +6,22 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.BREVO_EMAIL,
     pass: process.env.BREVO_SMTP_KEY,
   },
-  tls: {
-    rejectUnauthorized: false 
-  }
+  connectionTimeout: 10000,
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("Brevo SMTP Error:", error.message);
-  } else {
-    console.log("Brevo SMTP Server is ready to send emails");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("Brevo SMTP Error:", error.message);
+//   } else {
+//     console.log("Brevo SMTP Server is ready to send emails");
+//   }
+// });
 
 const ADMIN_EMAILS = ["admin@marketplace.com"];
 
